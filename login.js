0dc7ac1e7730cf7e43622bca55c4122ab5975c7c
Mbrain <ennocentcip@gmail.com>
2024-03-17 05:58:43 +0100

    include keypress func

--- login.js
+++ login.js
@@ -1,7 +1,15 @@
 const btn = document.querySelector('.click-btn');
 const modal = document.querySelector('.modal');
 
-btn.addEventListener('click', (e) => {
+btn.addEventListener('click', () => {
   // e.stopPropagation();
   modal.classList.add('show-modal');
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    if (modal.classList.contains('show-modal')) {
+      modal.classList.remove('show-modal');
+    }
+  }
 });
